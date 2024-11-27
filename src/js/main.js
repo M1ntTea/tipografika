b98cef241dadae9a805efd5a58405522c75eb49c
M1ntTea <147503917+M1ntTea@users.noreply.github.com>
2024-11-27 20:58:42 +0300

    Version 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
 document.body.querySelector('#hbgBtn').addEventListener('click', (event) => {
-    const menu = document.body.querySelector('#menu');
-    if (menu.style.display === 'none')
-       menu.style.display = 'initial';
-    else
-       menu.style.display = 'none';
+   const menu = document.body.querySelector('#menu');
+   const burger_open = document.body.querySelector('.burger__open');
+   const burger_close = document.body.querySelector('.burger__close');
+
+   if (menu.style.display === 'none') {
+      menu.style.display = 'flex';
+      burger_open.style.display = 'none';
+      burger_close.style.display = 'initial';
+   } else {
+      menu.style.display = 'none';
+      burger_open.style.display = 'initial';
+      burger_close.style.display = 'none';
+   }
  });
