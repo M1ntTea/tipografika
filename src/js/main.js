06cbdc9f824c640b812a8111c33abb9ef220fe22
M1ntTea <147503917+M1ntTea@users.noreply.github.com>
2024-11-29 20:24:37 +0300

    Version 3
    Десктоп на 1470 выглядит хорошо, но все остальное плохо, особенно мобильная версия.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,7 @@
-document.body.querySelector('#hbgBtn').addEventListener('click', (event) => {
-   const menu = document.body.querySelector('#menu');
-   const burger_open = document.body.querySelector('.burger__open');
-   const burger_close = document.body.querySelector('.burger__close');
-
-   if (menu.style.display === 'none') {
-      menu.style.display = 'flex';
-      burger_open.style.display = 'none';
-      burger_close.style.display = 'initial';
-   } else {
-      menu.style.display = 'none';
-      burger_open.style.display = 'initial';
-      burger_close.style.display = 'none';
+(function () {
+   const header = document.querySelector('.header');
+   const icon = document.querySelector('.icon-container');
+   icon.onclick = function () {
+   header.classList.toggle('menu-open');
    }
- });
+}());
